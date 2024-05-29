6b8e11dfcb0490308ee38bf07ee398c54f554f32
lallain <l.allain@omnipacgroup.com>
2024-05-29 09:12:15 +0200

    Ajoutez des fichiers projet.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.dataGridViewClients = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClients)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(100, 10);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 20);
+            this.txtNom.TabIndex = 0;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(100, 40);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(100, 20);
+            this.txtPrenom.TabIndex = 1;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(100, 70);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(10, 100);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 3;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(90, 100);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 4;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(170, 100);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSupprimer.TabIndex = 5;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // dataGridViewClients
+            // 
+            this.dataGridViewClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewClients.Location = new System.Drawing.Point(10, 140);
+            this.dataGridViewClients.Name = "dataGridViewClients";
+            this.dataGridViewClients.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewClients.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dataGridViewClients);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.txtNom);
+            this.Name = "Form1";
+            this.Text = "Gestion des Clients";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClients)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.DataGridView dataGridViewClients;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDatabaseConnection();
+            LoadClients();
+        }
+
+        private void InitializeDatabaseConnection()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ClientsDB"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+        }
+
+        private void LoadClients()
+        {
+            try
+            {
+                sqlConnection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Clients", sqlConnection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                dataGridViewClients.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du chargement des clients : " + ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO Clients (Nom, Prenom, Numero) VALUES (@Nom, @Prenom, @Numero)", sqlConnection);
+                command.Parameters.AddWithValue("@Nom", txtNom.Text);
+                command.Parameters.AddWithValue("@Prenom", txtPrenom.Text);
+                command.Parameters.AddWithValue("@Numero", txtNumero.Text);
+                command.ExecuteNonQuery();
+                LoadClients();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de l'ajout du client : " + ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewClients.SelectedRows.Count > 0)
+            {
+                int clientId = Convert.ToInt32(dataGridViewClients.SelectedRows[0].Cells["Id"].Value);
+                try
+                {
+                    sqlConnection.Open();
+                    SqlCommand command = new SqlCommand("UPDATE Clients SET Nom = @Nom, Prenom = @Prenom, Numero = @Numero WHERE Id = @Id", sqlConnection);
+                    command.Parameters.AddWithValue("@Nom", txtNom.Text);
+                    command.Parameters.AddWithValue("@Prenom", txtPrenom.Text);
+                    command.Parameters.AddWithValue("@Numero", txtNumero.Text);
+                    command.Parameters.AddWithValue("@Id", clientId);
+                    command.ExecuteNonQuery();
+                    LoadClients();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur lors de la modification du client : " + ex.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner un client à modifier.");
+            }
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewClients.SelectedRows.Count > 0)
+            {
+                int clientId = Convert.ToInt32(dataGridViewClients.SelectedRows[0].Cells["Id"].Value);
+                try
+                {
+                    sqlConnection.Open();
+                    SqlCommand command = new SqlCommand("DELETE FROM Clients WHERE Id = @Id", sqlConnection);
+                    command.Parameters.AddWithValue("@Id", clientId);
+                    command.ExecuteNonQuery();
+                    LoadClients();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur lors de la suppression du client : " + ex.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner un client à supprimer.");
+            }
+        }
+    }
+}
