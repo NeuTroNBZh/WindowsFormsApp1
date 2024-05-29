ce79bb7827c30b47f40f93e1e6e925b78ab3198d
lallain <l.allain@omnipacgroup.com>
2024-05-29 10:41:41 +0200

    Premier Test pour C# et SQL

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,38 +7,30 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private SqlConnection sqlConnection;
+        private string connectionString = "Server=PC-INFO\\SQLEXPRESS;Database=Louistest;User Id=sa;Password=Mdp56350;";
 
         public Form1()
         {
             InitializeComponent();
-            InitializeDatabaseConnection();
             LoadClients();
-        }
-
-        private void InitializeDatabaseConnection()
-        {
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ClientsDB"].ConnectionString;
-            sqlConnection = new SqlConnection(connectionString);
         }
 
         private void LoadClients()
         {
             try
             {
-                sqlConnection.Open();
-                SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Clients", sqlConnection);
-                DataTable dataTable = new DataTable();
-                dataAdapter.Fill(dataTable);
-                dataGridViewClients.DataSource = dataTable;
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Clients", sqlConnection);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    dataGridViewClients.DataSource = dataTable;
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Erreur lors du chargement des clients : " + ex.Message);
-            }
-            finally
-            {
-                sqlConnection.Close();
             }
         }
 
@@ -46,21 +38,22 @@ namespace WindowsFormsApp1
         {
             try
             {
-                sqlConnection.Open();
-                SqlCommand command = new SqlCommand("INSERT INTO Clients (Nom, Prenom, Numero) VALUES (@Nom, @Prenom, @Numero)", sqlConnection);
-                command.Parameters.AddWithValue("@Nom", txtNom.Text);
-                command.Parameters.AddWithValue("@Prenom", txtPrenom.Text);
-                command.Parameters.AddWithValue("@Numero", txtNumero.Text);
-                command.ExecuteNonQuery();
-                LoadClients();
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    using (SqlCommand command = new SqlCommand("INSERT INTO Clients (Nom, Prenom, Numero) VALUES (@Nom, @Prenom, @Numero)", sqlConnection))
+                    {
+                        command.Parameters.AddWithValue("@Nom", txtNom.Text);
+                        command.Parameters.AddWithValue("@Prenom", txtPrenom.Text);
+                        command.Parameters.AddWithValue("@Numero", txtNumero.Text);
+                        command.ExecuteNonQuery();
+                        LoadClients();
+                    }
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Erreur lors de l'ajout du client : " + ex.Message);
-            }
-            finally
-            {
-                sqlConnection.Close();
             }
         }
 
@@ -68,25 +61,26 @@ namespace WindowsFormsApp1
         {
             if (dataGridViewClients.SelectedRows.Count > 0)
             {
-                int clientId = Convert.ToInt32(dataGridViewClients.SelectedRows[0].Cells["Id"].Value);
+                int clientId = Convert.ToInt32(GetCellValue(dataGridViewClients.SelectedRows[0].Cells["Id"]));
                 try
                 {
-                    sqlConnection.Open();
-                    SqlCommand command = new SqlCommand("UPDATE Clients SET Nom = @Nom, Prenom = @Prenom, Numero = @Numero WHERE Id = @Id", sqlConnection);
-                    command.Parameters.AddWithValue("@Nom", txtNom.Text);
-                    command.Parameters.AddWithValue("@Prenom", txtPrenom.Text);
-                    command.Parameters.AddWithValue("@Numero", txtNumero.Text);
-                    command.Parameters.AddWithValue("@Id", clientId);
-                    command.ExecuteNonQuery();
-                    LoadClients();
+                    using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        using (SqlCommand command = new SqlCommand("UPDATE Clients SET Nom = @Nom, Prenom = @Prenom, Numero = @Numero WHERE Id = @Id", sqlConnection))
+                        {
+                            command.Parameters.AddWithValue("@Nom", txtNom.Text);
+                            command.Parameters.AddWithValue("@Prenom", txtPrenom.Text);
+                            command.Parameters.AddWithValue("@Numero", txtNumero.Text);
+                            command.Parameters.AddWithValue("@Id", clientId);
+                            command.ExecuteNonQuery();
+                            LoadClients();
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show("Erreur lors de la modification du client : " + ex.Message);
-                }
-                finally
-                {
-                    sqlConnection.Close();
                 }
             }
             else
@@ -99,28 +93,34 @@ namespace WindowsFormsApp1
         {
             if (dataGridViewClients.SelectedRows.Count > 0)
             {
-                int clientId = Convert.ToInt32(dataGridViewClients.SelectedRows[0].Cells["Id"].Value);
+                int clientId = Convert.ToInt32(GetCellValue(dataGridViewClients.SelectedRows[0].Cells["Id"]));
                 try
                 {
-                    sqlConnection.Open();
-                    SqlCommand command = new SqlCommand("DELETE FROM Clients WHERE Id = @Id", sqlConnection);
-                    command.Parameters.AddWithValue("@Id", clientId);
-                    command.ExecuteNonQuery();
-                    LoadClients();
+                    using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        using (SqlCommand command = new SqlCommand("DELETE FROM Clients WHERE Id = @Id", sqlConnection))
+                        {
+                            command.Parameters.AddWithValue("@Id", clientId);
+                            command.ExecuteNonQuery();
+                            LoadClients();
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show("Erreur lors de la suppression du client : " + ex.Message);
-                }
-                finally
-                {
-                    sqlConnection.Close();
                 }
             }
             else
             {
                 MessageBox.Show("Veuillez sélectionner un client à supprimer.");
             }
+        }
+
+        private object GetCellValue(DataGridViewCell cell)
+        {
+            return cell.Value == DBNull.Value ? null : cell.Value;
         }
     }
 }
